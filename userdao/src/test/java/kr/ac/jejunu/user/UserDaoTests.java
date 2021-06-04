f266830cae56a64f8369255bf28336c3ac49c1b4
hshk1004 <hshk_1004@naver.com>
2021-06-04 11:13:55 +0900

    사용자 이름, 패스워드 추가하는 메서드 추가

--- userdao/src/test/java/kr/ac/jejunu/user/UserDaoTests.java
+++ userdao/src/test/java/kr/ac/jejunu/user/UserDaoTests.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.Matchers.greaterThan;
 
 public class UserDaoTests {
     @Test
@@ -19,4 +20,21 @@ public class UserDaoTests {
         assertThat(user.getName(), is(name));
         assertThat(user.getPassword(), is(password));
     }
+
+    @Test
+    public void insert() throws SQLException, ClassNotFoundException {
+        String name = "난가?";
+        String password = "3456";
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        UserDao userDao = new UserDao();
+        userDao.insert(user);
+        User insertedUser = userDao.findById(user.getId());
+        assertThat(user.getId(), greaterThan(0));
+        assertThat(insertedUser.getId(), is(user.getId()));
+        assertThat(insertedUser.getName(), is(user.getName()));
+        assertThat(insertedUser.getPassword(), is(user.getPassword()));
+    }
+
 }
